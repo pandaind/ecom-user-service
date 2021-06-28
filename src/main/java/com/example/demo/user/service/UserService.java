2c044d64238e97f7e885ed424bf0dd5d3826fcf5
chittaranjanpanda <chittaranjan@hotmail.com>
2021-06-28 23:21:27 +0530

    User Service

--- src/main/java/com/example/demo/user/service/UserService.java
+++ src/main/java/com/example/demo/user/service/UserService.java
@@ -1,3 +1,15 @@
 package com.example.demo.user.service;
 
-public interface UserService {}
+import com.example.demo.user.service.dto.UserDTO;
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+  List<UserDTO> findAll();
+
+  Optional<UserDTO> findOne(Long id);
+
+  UserDTO findOneByName(String userName);
+
+  UserDTO save(UserDTO user);
+}
--- src/main/java/com/example/demo/user/service/mapper/UserMapper.java
+++ src/main/java/com/example/demo/user/service/mapper/UserMapper.java
@@ -8,7 +8,9 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 /** Mapper for the entity {@link User} and its DTO {@link UserDTO}. */
-@Mapper(componentModel = "spring")
+@Mapper(
+    componentModel = "spring",
+    uses = {})
 public interface UserMapper extends EntityMapper<UserDTO, User> {
   @Named("id")
   @BeanMapping(ignoreByDefault = true)
